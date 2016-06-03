4af862c98e1c6fbe7477b2dc13a938b36abe5fb9
Lea de Groot <l.degroot@library.uq.edu.au>
2016-06-03 10:57:09 +1000

    PT120410419 Updating error messages

--- elements/uqlibrary-time-selector.js
+++ elements/uqlibrary-time-selector.js
@@ -18,7 +18,7 @@
         notify: true,
         value: 0
       },
-      timeslot: { observer: 'timeslotChanged' }
+      timeslot: {observer: 'timeslotChanged'}
     },
     /**
      * Returns an aria label for a time slot
@@ -48,15 +48,15 @@
 
       // Check if the time slot change is valid
       result = this._timeSlotChangeValid(selectedIndex);
-      if (true === result) {
+      if (true === result.isValid) {
         selectedItem.selected = !selectedItem.selected;
 
         // This is required for Polymer to update this object and bubble it up
         this.bookingTimeSlots = this.bookingTimeSlots.slice();
         this.notifyPath('bookingTimeSlots.' + selectedIndex + '.selected', selectedItem.selected);
       } else {
-          this.$.toast.text = result;
-          this.$.toast.show();
+        this.$.toast.text = result.errorMessage;
+        this.$.toast.show();
       }
     },
     /**
@@ -70,40 +70,48 @@
       var previous = (index > 0 ? this.bookingTimeSlots[index - 1] : null);
       var next = ((index + 1) < this.bookingTimeSlots.length ? this.bookingTimeSlots[index + 1] : null);
 
-      var selectedItems = _.filter(this.bookingTimeSlots, function (value) { return value.selected; });
+      var selectedItems = _.filter(this.bookingTimeSlots, function (value) {
+        return value.selected;
+      });
 
       // Check if the item is selectable
-      result = true;
+      var isValid = true;
+      var errorMessage = "";
+
       if (!item.selectable) {
-        result = "Time slot cannot be selected";
+        errorMessage = "Time slot cannot be selected";
+        isValid = false;
+        return {isValid: isValid, errorMessage: errorMessage};
       }
 
-      if (true == result) {
-        if (item.selected) {
-          // User wants to deselect, make sure it is not in the middle of the selected area
-          if (previous && previous.selected && next && next.selected) {
-            result = "You cannot turn off a time slot in the middle of the time block";
-          }
-        } else {
-          // User wants to select this item
-          // If no elements have been selected, it is valid
-          if (selectedItems.length > 0) {
+      if (item.selected) {
+        // User wants to deselect, make sure it is not in the middle of the selected area
+        if (previous && previous.selected && next && next.selected) {
+          errorMessage = "Bookings must be continuous timeslots";
+          isValid = false;
+        }
+      } else {
+        // User wants to select this item
+        // If no elements have been selected, it is valid
+        if (selectedItems.length > 0) {
 
-            // Make sure at least one time slot next to this item is selected
-            if (previous && previous.selected || next && next.selected) {
-              if (this.maxBookingLength != 0 && selectedItems.length + 1 > this.maxBookingLength) {
-                // max booking length reached
-                result = "Maximum Length of booking reached"
-              }
-            } else {
-              // non contiguous time selected
-              result = "Create a second booking to choose a second block of time";
+          // Make sure at least one time slot next to this item is selected
+          if (previous && previous.selected || next && next.selected) {
+            if (this.maxBookingLength != 0 && selectedItems.length + 1 > this.maxBookingLength) {
+              // max booking length reached
+              errorMessage = "Maximum length of booking reached";
+              isValid = false;
             }
+          } else {
+            // non contiguous time selected
+            errorMessage = "Turn off the current time slot(s) to pick a new time";
+            isValid = false;
           }
         }
       }
-      return result;
+
+      return {isValid: isValid, errorMessage: errorMessage};
     }
-  }); 
+  });
 
 })();
